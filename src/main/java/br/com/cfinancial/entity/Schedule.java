62df9a3ea3668f3a99a99f6a1e8f68974c4bc4dc
Willian Domingues <Willian Domingues@192.168.15.13>
2018-04-05 23:58:08 -0300

    Criação dos parâmetros das entidades Schedule e User

--- src/main/java/br/com/cfinancial/entity/Schedule.java
+++ src/main/java/br/com/cfinancial/entity/Schedule.java
@@ -1,5 +1,90 @@
 package br.com.cfinancial.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Escala")
 public class Schedule {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "cf_escala_mes")
+	private int mes;
+	
+	@Column(name = "cf_escala_ano")
+	private int ano;
+	
+	@Column(name = "cf_escala_avisos")
+	private String aviso;
+	
+	@Column(name = "cf_escala_evento_id")
+	private int evento_id;
+	
+	@Column(name = "cf_escala_dtinclusao")
+	private Date dtinclusao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getAviso() {
+		return aviso;
+	}
+
+	public void setAviso(String aviso) {
+		this.aviso = aviso;
+	}
+
+	public int getEvento_id() {
+		return evento_id;
+	}
+
+	public void setEvento_id(int evento_id) {
+		this.evento_id = evento_id;
+	}
+
+	public Date getDtinclusao() {
+		return dtinclusao;
+	}
+
+	public void setDtinclusao(Date dtinclusao) {
+		this.dtinclusao = dtinclusao;
+	}
+
+	@Override
+	public String toString() {
+		return "Schedule [id=" + id + ", mes=" + mes + ", ano=" + ano + ", aviso=" + aviso + ", evento_id=" + evento_id
+				+ "]";
+	}
+
 }
+
--- src/main/java/br/com/cfinancial/entity/User.java
+++ src/main/java/br/com/cfinancial/entity/User.java
@@ -1,5 +1,90 @@
 package br.com.cfinancial.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Usuario")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "cf_usuario_login")
+	private String login;
+	
+	@Column(name = "cf_usuario_nome")
+	private String nome;
+	
+	@Column(name = "cf_usuario_senha")
+	private String senha;
+	
+	@Column(name = "cf_usuario_tipo")
+	private String tipo;
+	
+	@Column(name = "cf_usuario_dtinclusao")
+	private Date dt_inclusao;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Date getDt_inclusao() {
+		return dt_inclusao;
+	}
+
+	public void setDt_inclusao(Date dt_inclusao) {
+		this.dt_inclusao = dt_inclusao;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", login=" + login + ", nome=" + nome + ", tipo=" + tipo + "]";
+	}
+	
 
 }
